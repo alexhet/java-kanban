7eec709842287f1c59d523f5febe1209d1c1ed3d
Alexandr Zhuchkov <alexandrzhuchkov69@gmail.com.com>
2025-02-04 19:37:26 +0300

    Добавить новых тестов

--- src/tests/HistoryManagerTest.java
+++ src/tests/HistoryManagerTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import managers.HistoryManager;
 import managers.InMemoryHistoryManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tasks.Status;
 import tasks.Task;
@@ -11,9 +12,15 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HistoryManagerTest {
+    HistoryManager historyManager;
+
+    @BeforeEach
+    void setUp() {
+        historyManager = new InMemoryHistoryManager(); 
+    }
+
     @Test
     void historyMaintainsPreviousTaskState() {
-        HistoryManager historyManager = new InMemoryHistoryManager();
         Task task = new Task("Задача", "Описание", Status.NEW);
         task.setId(1);
         historyManager.add(task);
@@ -21,5 +28,32 @@ class HistoryManagerTest {
         List<Task> history = historyManager.getHistory();
         assertEquals(1, history.size(), "История должна содержать одну задачу.");
         assertEquals(task, history.get(0), "История должна содержать корректную задачу.");
+    }
+
+    @Test
+    void testRemoveTask() {
+        Task task1 = new Task("Задача1", "Описание1", Status.NEW);
+        task1.setId(1);
+        Task task2 = new Task("Задача2", "Описание2", Status.NEW);
+        task2.setId(2);
+
+        historyManager.add(task1);
+        historyManager.add(task2);
+
+        historyManager.remove(task2.getId());
+
+        List<Task> history = historyManager.getHistory();
+
+        assertEquals(1, history.size(), "После удаления одной задачи в истории должна остаться одна задача.");
+        assertEquals(task1, history.get(0), "В истории должна остаться задача task1.");
+    }
+
+    @Test
+    void testAddTask() {
+        Task task = new Task("Задача", "Описание", Status.NEW);
+        historyManager.add(task);
+
+        assertEquals(1, historyManager.getHistory().size());
+        assertEquals(task, historyManager.getHistory().get(0));
     }
 }
